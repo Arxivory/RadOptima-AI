b13a135e51743fc8a2b78a04728a0acf58ee64aa
Vince <babyaivan86@gmail.com>
2026-02-19 18:18:27 +0800

    Made the volume uploading function.

--- cpp_core/main.cu
+++ cpp_core/main.cu
@@ -1,21 +1,46 @@
 #include <cuda_runtime.h>
 #include <iostream>
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+using namespace std;
 
 namespace py = pybind11;
 
-void check_cuda() {
-    int deviceCount = 0;
-    cudaGetDeviceCount(&deviceCount);
-    if (deviceCount == 0) {
-        std::cout << "No CUDA devices found!" << std::endl;
+class RadEngine {
+public:
+    RadEngine() {
+        cout << "Engine Initialized." << endl;
     }
-    else {
-        std::cout << "RadOptima Engine: Found " << deviceCount << " GPU(s)." << std::endl;
+
+    void upload_volume(py::array_t<int16_t> numpy_volume) {
+        py::buffer_info buf = numpy_volume.request();
+
+        if (buf.ndim != 3) {
+            throw runtime_error("Volume be a 3D array (Z, Y, X");
+        }
+
+        int depth = (int)buf.shape[0];
+        int height = (int)buf.shape[1];
+        int width = (int)buf.shape[2];
+
+        cout << "C++ Received Volume: " << width << "x" << height << "x" << depth << endl;
+
+        int16_t* ptr = static_cast<int16_t*>(buf.ptr);
+
+        cout << "First voxel value: " << ptr[0] << " HU" << endl;
+    
     }
-}
+};
 
 PYBIND11_MODULE(radoptima_core, m) {
-    m.doc() = "RadOptima AI CUDA-OpenGL Engine";
-    m.def("check_cuda", &check_cuda, "A function to check GPU availability");
+    py::class_<RadEngine>(m, "RadEngine")
+        .def(py::init<>())
+        .def("upload_volume", &RadEngine::upload_volume);
+
+    m.def("check_cuda", []() {
+        int count;
+        cudaGetDeviceCount(&count);
+        cout << "CUDA Devices: " << count << endl;
+    });
 }
