3b8f0caeebc95f7d6c1de3bea9a0e97d270f30d7
Vince <babyaivan86@gmail.com>
2026-02-19 18:33:04 +0800

    Made the C++ handshake with Python by accessing data.

--- test_engine.py
+++ test_engine.py
@@ -1,7 +1,8 @@
 import sys
 import os
+import numpy as np
 
-build_path = os.path.join(os.getcwd(), "out", "build", "x64-Release", "cpp_core")
+build_path = os.path.join(os.getcwd(), "out", "build", "x64-Debug", "cpp_core")
 
 if os.path.exists(build_path):
     sys.path.append(build_path)
@@ -10,11 +11,19 @@ else:
     print(f"Error: Could not find build folder at {build_path}")
     print("Make sure you have clicked 'Build All' in Visual Studio.")
 
+from data.dicom_loader import load_dicom_volume
+
 try:
     import radoptima_core
-    print("Successfully imported RadOptima Core!")
-    
-    radoptima_core.check_cuda()
+
+    data_path = "data/samples/test_scan"
+    volume = load_dicom_volume(data_path)
+
+    engine = radoptima_core.RadEngine()
+
+    engine.upload_volume(volume)
+
+    print("Handshake successful! C++ has access to the DICOM pixels.")
     
 except ImportError as e:
     print(f"Import failed: {e}")
